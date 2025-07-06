92f92d0f6a3b2075d6854ed9fccdfee74a357f8b
Mahdi <mahdimomeni6@gmail.com>
2025-07-06 21:12:20 +0330

    feat: wait for user connection

--- app/main.go
+++ app/main.go
@@ -6,22 +6,21 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	// wait for a connection
+	conn, err := l.Accept()
+
+	conn.Write([]byte("PONG\r\n"))
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
