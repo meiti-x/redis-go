03def74768b2d36108f7c3367e235e2752660860
Mahdi <mahdimomeni6@gmail.com>
2025-07-07 21:53:38 +0330

    feat: redis ping pong

--- app/main.go
+++ app/main.go
@@ -1,28 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 var _ = net.Listen
 var _ = os.Exit
 
-func main() {
-	fmt.Println("Logs from your program will appear here!")
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
 
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		msg := strings.TrimSpace(scanner.Text())
+		if strings.ToUpper(msg) == "PING" {
+			conn.Write([]byte("PONG\r\n"))
+		}
+	}
+}
+func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	// wait for a connection
-	conn, err := l.Accept()
 
-	conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
+
 }
